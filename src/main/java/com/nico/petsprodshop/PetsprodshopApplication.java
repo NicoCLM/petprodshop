53f64069d359a130708762690f084a691c75269f
NicoCLM <nc090202@gmail.com>
2024-07-01 08:31:46 -0500

    Modulo Persona en Spring

---
+++ src/main/java/com/nico/petsprodshop/PetsprodshopApplication.java
@@ -0,0 +1,13 @@
+package com.nico.petsprodshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetsprodshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetsprodshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nico/petsprodshop/PetsprodshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nico.petsprodshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetsprodshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
